adca5d27047b515e0d72c79a8c68b3bab2a255a2
joukey123 <0162943362@naver.com>
2023-12-06 16:35:47 +0900

    min->hour with selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import "./App.css";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hourSelector, minuteState } from "./Atoms";
 
 function App() {
-  return null;
+  const [minute, setMinute] = useRecoilState(minuteState);
+  const hour = useRecoilValue(hourSelector);
+
+  const changeMinute = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinute(+event.currentTarget.value);
+  };
+  return (
+    <div>
+      <input
+        value={minute}
+        onChange={changeMinute}
+        type="number"
+        placeholder="minutes"
+      />
+      <input value={hour} type="number" placeholder="hours" />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/theme.tsx
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components/dist/types";
+
+export const theme: DefaultTheme = {
+  bgColor: "#1e272e",
+  textColor: "#d2dae2",
+  accentColor: "#f53b57",
+};
